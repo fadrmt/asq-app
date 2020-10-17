0e394866251ccacbdd83285f1b5773e0be05929c
fadrmt <faraladat@gmail.com>
2020-10-17 18:53:31 +0800

    [FIX] Remove unique limit on posted_by (User)

--- api/asq/models.py
+++ api/asq/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     title = models.CharField(max_length=150, verbose_name='Question Title', unique=True)
     body = models.TextField(verbose_name='Question Body')
     # Name of User - currently saved as text given there's no authentication
-    posted_by = models.CharField(max_length=100, verbose_name='User Name', unique=True)
+    posted_by = models.CharField(max_length=100, verbose_name='User Name')
     posted_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Question(models.Model):
 class Answer(models.Model):
     body = models.TextField(verbose_name='Answer Body')
     # Name of User - currently saved as text given there's no authentication
-    posted_by = models.CharField(max_length=100, verbose_name='User Name', unique=True)
+    posted_by = models.CharField(max_length=100, verbose_name='User Name')
     posted_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
