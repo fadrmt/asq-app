e38a6dd22e5e93df3be6244e7228d420435dd297
fadrmt <farshana@apollo.com.ph>
2020-10-18 11:25:44 +0800

    [UPDATE] Added axios call for retrieving question list

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import QuestionList from './QuestionList';
 
 class Home extends Component {
     state = {
-        questions: [
-            {
-                "id": 1,
-                "answer_count": 1,
-                "title": "What is the answer to life, the universe, and everything?",
-                "body": "Someone asked me this, and I'm stumped. Please help.",
-                "author_name": "Arthur Dent",
-                "date_posted": "2020-10-17T19:51:07.609614+08:00"
-            },
-            {
-                "id": 2,
-                "answer_count": 1,
-                "title": "What is the answer to life, the universe, and everything?",
-                "body": "Someone asked me this, and I'm stumped. Please help.",
-                "author_name": "Arthur Dent",
-                "date_posted": "2020-10-17T19:51:07.609614+08:00"
-            }
-        ]
+        questions: []
     }
+
+    componentDidMount() {
+        this.getQuestionList()
+    }
+
+    getQuestionList = () => {
+        axios.get('http://localhost:8000/api/questions/')
+          .then(res => {
+            console.log(res.data)
+            this.setState({
+              questions: res.data
+            })
+        })
+    }
+    
     render() {
         return (
             <>
--- frontend/src/components/QuestionList.js
+++ frontend/src/components/QuestionList.js
@@ -5,14 +5,19 @@ import QuestionListItem from './QuestionListItem';
 
 const QuestionList = (props) => {
   const { questions } = props
-  const questionList = questions.map(question => {
-    return (
-        <Card hoverable key={questions.id} className="question-card">
-            <QuestionListItem question={question}/>
-        </Card>
-    )
-  })
+  const questionList = questions.length ? (
+        questions.map(question => {
+            return (
+                <Card hoverable key={questions.id} className="question-card">
+                    <QuestionListItem question={question}/>
+                </Card>
+            )
+        })
+  ) : (
+      <p>No questions posted.</p>
+  )
   return questionList;
+  
 }
  
 export default QuestionList;
